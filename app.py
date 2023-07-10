63fbaa9b14a1d003d09240ff305052474f452cbf
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-07-09 23:24:40 -0300

    ta quase

--- app.py
+++ app.py
@@ -4,17 +4,48 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
-def helloWorld():
+def index():
     con = sqlite3.connect('database.db')
     cursor = con.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS musicas")
     cursor.execute("CREATE TABLE musicas(titulo text, banda text, album text, ano text)")
     con.commit()
-    
-    res = cursor.execute("SELECT * FROM musicas")
+    con.close()
     
     return render_template('index.html')
+
+@app.route("/add", methods=['POST'])
+def tabela():
+    con = sqlite3.connect('database.db')
+    cursor = con.cursor()
+
+    data = request.form
+
+    titulo = data['titulo']
+    banda = data['banda']
+    album = data['album']
+    ano = data['ano']
+
+    sql = '''
+    INSERT INTO musicas (titulo, banda, album, ano)
+    VALUES (?, ?, ?, ?)
+    '''
+
+    cursor.execute(sql,[titulo, banda, album, ano])
+    cursor.execute('SELECT * FROM musicas')
+
+    con.commit()
+
+    musicArray = cursor.fetchall()
+    res = ''
+
+    for n in musicArray:
+        res += f'''
+        <p>Música: {n[0]}, Banda: {n[1]}, Álbum: {n[2]}, Ano de Lançamento: {n[3]}</p>
+        '''
+
+    return f'<h1>Resultado</h1> {musicArray}'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,25 @@
-bntAddItem = document.getElementById('add-item')
-form = document.getElementById('add')
+const addItemBtn = document.querySelector('#add-item')
+const submitBtn = document.querySelector('#submit')
+const closeBtn = document.querySelector('#close')
+
+const form = document.querySelector('form')
+const addItemText = document.querySelector('p')
+const inputs = [...document.querySelectorAll('input[type="text"]')]
+
+addItemBtn.addEventListener('click', () => {
+    form.style.display = 'flex'
+    addItemText.style.display = 'none'
+})
+
+closeBtn.addEventListener('click', () => {
+    form.style.display = 'none'
+    addItemText.style.display = 'block'
+
+    form.reset()
+    }
+)
+
+submitBtn.addEventListener('click', () => {
+    form.submit()
+    console.log('ola')
+})
