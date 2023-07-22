9417b8a06d024c3b15dad12705e65c901b56675b
Filipe Silva <filipe.cavalcante0411@gmail.com>
2023-07-22 17:33:00 -0300

    att

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,13 @@
 const addItemBtn = document.querySelector('#add-item')
 const submitBtn = document.querySelector('#submit')
 const closeBtn = document.querySelector('#close-form')
-const updateBtn = document.querySelector('#update')
 
 const main = document.querySelector('main')
 const form = document.querySelector('form')
-const updateForm = document.querySelector('#update-form')
 const addItemText = document.querySelector('p')
 const inputs = [...document.querySelectorAll('input[type="text"]')]
 const table = document.querySelector('table')
+const tableContent = document.querySelector('tbody')
 
 addItemBtn.addEventListener('click', () => {
     form.style.display = 'flex'
@@ -27,6 +26,13 @@ submitBtn.addEventListener('click', () => {
     form.submit()
 })
 
-updateBtn.addEventListener('click', () => {
-    updateForm.submit()
-})
+const verifyContent = () => {
+    if (tableContent.children.length == 0) {
+        table.style.display = 'none'
+    } else {
+        table.style.display = 'block'
+    }
+}
+
+verifyContent()
+
---
+++ static/js/update.js
@@ -0,0 +1,6 @@
+const updateBtn = document.querySelector('#update')
+const updateForm = document.querySelector('#update-form')
+
+updateBtn.addEventListener('click', () => {
+    updateForm.submit()
+})
